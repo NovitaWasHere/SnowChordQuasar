998a5da0903b52fa0df860a76eb176ab29a0f112
Novita <novitawashere@gmail.com>
2024-01-25 12:21:37 +0100

    Se actualizo el profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,2 +1,119 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<template>dasdasdas</template>
+<template>
+  <div class="flex flex-center my-fontAcme q-pb-xl">
+    <div
+      class="bg-white q-mt-xl"
+      style="
+        width: 60%;
+        border: 5px solid black;
+        border-radius: 0px 45px 45px 0px;
+      "
+    >
+      <div>
+        <div
+          class="bg-accent q-mt-xl"
+          style="
+            width: 60%;
+            border: 5px solid black;
+            border-radius: 0px 45px 45px 0px;
+            color: white;
+          "
+        >
+          <h2 class="q-pl-xl">Nombre de Usuario</h2>
+        </div>
+      </div>
+      <div class="flex flex-center q-pt-xl">
+        <div class="flex flex-center" style="width: 30%">
+          <img src="~assets/Img/LogoSnow.png" alt="" />
+        </div>
+      </div>
+      <div class="column">
+        <div class="row">
+          <div class="col flex flex-center">
+            <div class="flex flex-center">
+              <h3>
+                Creditos
+                <h4 class="col flex flex-center">
+                  <q-icon class="q-mr-xl" name="ac_unit" />0
+                </h4>
+              </h3>
+            </div>
+          </div>
+          <div class="col flex flex-center">
+            <div class="flex flex-center">
+              <h3>
+                Faciles
+                <h4 class="col flex flex-center">
+                  <div style="width: 40px" class="bg-green q-mr-xl radius">
+                    ‎
+                  </div>
+                  0
+                </h4>
+              </h3>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col flex flex-center">
+            <div class="flex flex-center">
+              <h3>
+                Perfectos
+                <h4 class="col flex flex-center">
+                  <q-icon class="q-mr-xl" name="star" />0
+                </h4>
+              </h3>
+            </div>
+          </div>
+          <div class="col flex flex-center">
+            <div class="flex flex-center">
+              <h3>
+                Medios
+                <h4 class="col flex flex-center">
+                  <div style="width: 40px" class="bg-yellow q-mr-xl radius">
+                    ‎
+                  </div>
+                  0
+                </h4>
+              </h3>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col flex flex-center">
+            <div class="flex flex-center">
+              <h3>
+                Canciones
+                <h4 class="col flex flex-center">
+                  <q-icon class="q-mr-xl" name="headset_mic" />0
+                </h4>
+              </h3>
+            </div>
+          </div>
+          <div class="col flex flex-center">
+            <div class="flex flex-center">
+              <h3>
+                Dificiles
+                <h4 class="col flex flex-center">
+                  <div style="width: 40px" class="bg-red q-mr-xl radius">‎</div>
+                  0
+                </h4>
+              </h3>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col flex flex-center">
+            <div class="flex flex-center">
+              <h3>
+                Juegos Terminados
+                <h4 class="col flex flex-center">
+                  <q-icon class="q-mr-xl" name="gamepad" />0
+                </h4>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,7 +1,9 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
+  <headHome></headHome>
   <Profile></Profile>
 </template>
 <script setup>
+import headHome from "src/components/headHome.vue";
 import Profile from "src/components/Profile.vue";
 </script>
